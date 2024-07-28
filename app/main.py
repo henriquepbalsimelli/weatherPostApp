cdfa04c464e726148bf152388ba324fccf413245
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2024-07-28 11:02:51 -0300

    feat: adicionando loaddotenv

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 from .routers.publications import x_publications
 
 app = FastAPI()
 
 app.include_router(x_publications.router)
+
+dotenv_path = join(dirname(dirname(dirname(__file__))), '.env')
+load_dotenv(dotenv_path)
 
 @app.get("/")
 async def root():
