a12705495821335b441bba4336178150f9f8410a
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2024-07-28 13:50:19 -0300

    feat: criando try catch na .env

--- configurations.py
+++ configurations.py
@@ -3,4 +3,7 @@ from dotenv import dotenv_values
 envs = dotenv_values(".env")
 
 def get_env(env_name: str):
-    return envs.get(env_name)
+    env = envs.get(env_name, None)
+    if not env:
+        raise EnvironmentError
+    return env
